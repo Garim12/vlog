032b3cb83dd2edddb0aec7d35b23fa7892ccbd9b
MSM <qw12345611@naver.com>
2023-07-14 07:00:23 +0900

    중간 저장 좋아요 기능추가, valid 수정

--- src/main/java/com/sparta/vlog/dto/SignupRequestDto.java
+++ src/main/java/com/sparta/vlog/dto/SignupRequestDto.java
@@ -10,7 +10,7 @@ import lombok.Setter;
 public class SignupRequestDto {
     @Pattern(regexp = "^[a-z0-9]{4,10}$")
     private String username;
-    @Pattern(regexp = "^[A-Za-z0-9]{4,10}$")
+    @Pattern(regexp = "^[A-Za-z0-9!@#$%^&*()_+{}:\"<>?,.\\\\/]{8,15}$")
     private String password;
     @Pattern(regexp = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,6}$", message = "이메일의 형식이 올바르지 않습니다. 다시 작성해주세요.")
     @NotBlank
